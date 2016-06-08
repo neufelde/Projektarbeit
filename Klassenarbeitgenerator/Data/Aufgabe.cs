0b8f8d1f52e595941ae1aafe437aef1806826255
Bratonatz <danielibach@web.de>
2016-06-08 11:43:39 +0200

    Aufgabe/Klassenarbeit Klasse

---
+++ Klassenarbeitgenerator/Data/Aufgabe.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Klassenarbeitgenerator.Data
+{
+    class Aufgabe
+    {
+        SQLCommands sqlCommander = new SQLCommands();
+
+        public Aufgabe(string name, int schwierigkeitsgrad, string thema, string fach, string aufgabentext, DateTime erstelldatum)
+        {
+            this.name = name;
+            this.schwierigkeitsgrad = schwierigkeitsgrad;
+            this.thema = thema;
+            this.fach = fach;
+            this.aufgabentext = aufgabentext;
+            this.erstelldatum = erstelldatum;
+        }
+
+        public Aufgabe(string name, int schwierigkeitsgrad, string thema, string fach, string aufgabentext, DateTime erstelldatum, string bild)
+        {
+            this.name = name;
+            this.schwierigkeitsgrad = schwierigkeitsgrad;
+            this.thema = thema;
+            this.fach = fach;
+            this.aufgabentext = aufgabentext;
+            this.bild = Image.FromFile(bild);
+            this.erstelldatum = erstelldatum;
+        }
+
+
+        private string name;
+        private int schwierigkeitsgrad;
+        private string thema;
+        private string fach;
+        private string aufgabentext;
+        private Image bild;
+        private DateTime erstelldatum;
+
+
+        public void AufgabeErstellen()
+        {
+
+        }
+    }
+}
---
+++ Klassenarbeitgenerator/Data/SQLCommands.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Data.SqlClient;
+
+namespace Klassenarbeitgenerator.Data
+{
+    class SQLCommands
+    {
+        public SQLCommands()
+        {
+            using (conn = new SqlConnection())
+            {
+                conn.ConnectionString = "Server=[ftp.strato.de];Database=[DB2546344];User id=ftp_dani@daniel-ibach.de;Password=sHg87$43bD;";
+                conn.Open();
+            }
+        }
+
+        private SqlConnection conn;
+
+        public void AddToDatabase(string name, int schwierigkeitsgrad, string thema, string fach, string aufgabentext, DateTime erstelldatum, Image bild)
+        {
+            conn.CreateCommand();
+        }
+    }
+}
